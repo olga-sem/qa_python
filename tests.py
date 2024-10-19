5204f4f03b4f6be4acf7c983d59ffcd55518791e
Olga Semenova <semenovaolga-2010@mail.ru>
2024-10-19 22:04:39 +0300

    book_and_genre

--- tests.py
+++ tests.py
@@ -1,34 +1,27 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        assert len(collector.get_books_genre()) == 2
 
     def test_add_new_book_over_40_symbols(self):
         collector = BooksCollector()
         collector.add_new_book('Клуб любителей книг и пирогов из картофельных очистков')
-        assert len(collector.get_books_rating()) != 3
+        assert len(collector.get_books_genre()) != 3
 
     def test_add_new_book_two_times(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         assert 'Гордость и предубеждение и зомби' not in collector
+
+    def test_set_book_genre_existing_book_and_existing_genre(self):
+        collector = BooksCollector()
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
+        assert 'Гордость и предубеждение и зомби' == 'Фантастика'
+
+    def test_set_book
